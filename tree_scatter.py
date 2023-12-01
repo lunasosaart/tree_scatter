55be9bec9982a0d761c5a74cc6442ba8dde37d60
lunasosaart <152564879+lunasosaart@users.noreply.github.com>
2023-11-30 19:00:28 -0500

    Update tree_scatter.py

--- tree_scatter.py
+++ tree_scatter.py
@@ -18,8 +18,6 @@ import random
 
 PFX = "LS_"
 
-# MAIN #
-
 
 def mk_tree1(itt):
     # create tree base #
@@ -30,6 +28,7 @@ def mk_tree1(itt):
     cmds.scale(1.3, 4.5, 1.3)
     cmds.DeleteHistory()
     cmds.FreezeTransformations()
+
 
     cmds.select(f"{PFX}tree1_{itt:02}_base1")
     cmds.scale(1.3, .3, 1.3)
@@ -71,7 +70,7 @@ def mk_tree1(itt):
         cmds.FreezeTransformations()
 
     # create leaves #
-    cmds.polySphere(n=f"{PFX}tree1_{itt:02}_leaf1", r=2, sx=8, sy=8)  # leaf 1
+    cmds.polySphere(n=f"{PFX}tree1_{itt:02}_leaf1", r=2, sx=8, sy=8) # leaf 1
     cmds.move(2.26, 17.7, 0, r=1)
     cmds.select(f"{PFX}tree1_{itt:02}_leaf1.f[21]", f"{PFX}tree1_{itt:02}_leaf1.f[29]",
                 f"{PFX}tree1_{itt:02}_leaf1.f[36]")
@@ -96,7 +95,7 @@ def mk_tree1(itt):
 
     # create other blocks
     cmds.select(f"{PFX}tree1_{itt:02}_leaf1", f"{PFX}tree1_{itt:02}_leaf2", f"{PFX}tree1_{itt:02}_leaf3")
-    cmds.duplicate()  # leaf 4, 5, 6
+    cmds.duplicate() # leaf 4, 5, 6
     cmds.move(-3, -7, -2, r=1)
     # cmds.rotate(-45,60,-70)
     cmds.select(f"{PFX}tree1_{itt:02}_leaf5")
@@ -121,13 +120,7 @@ def mk_tree1(itt):
     # create shader, apply, and group leaf bits
     leaf_grp = cmds.group(empty=1, n=f"{PFX}tree1_{itt:02}_leaves")
     leaf_obj_color = cmds.shadingNode('standardSurface', n=f"{PFX}tree3_{itt:02}_leaf_shd", asShader=1)
-    
-    # REPLACE WITH RANGE OF VALUES ONCE YOU GET HOME #
-    # randomize leaf hue
-    r_color = random.uniform(0.13)
-    g_color = random.uniform(.05)
-    b_color = random.uniform(.15)
-    cmds.setAttr(leaf_obj_color + ".baseColor",r_color, g_color, b_color)
+    cmds.setAttr(leaf_obj_color + ".baseColor", 0.13, .05, .15)
 
     for j in range(9):
         leaf = f"{PFX}tree1_{itt:02}_leaf" + str(j + 1)
@@ -138,7 +131,7 @@ def mk_tree1(itt):
         cmds.hyperShade(assign=leaf_obj_color)
 
     # cleanup
-    cmds.select(f"{PFX}tree1_{itt:02}_base", f"{PFX}tree1_{itt:02}_leaves")
+    cmds.select(f"{PFX}tree1_{itt:02}_base",f"{PFX}tree1_{itt:02}_leaves")
     cmds.DeleteHistory()
     cmds.FreezeTransformations()
 
@@ -146,12 +139,13 @@ def mk_tree1(itt):
     cmds.parent(f"{PFX}tree1_{itt:02}_leaves", f"{PFX}tree1_{itt:02}_base")
     cmds.parent(f"{PFX}tree1_{itt:02}_base", f"{PFX}tree1_{itt:02}_GRP")
 
+
     return cmds.move(0, 0, 0, curr_grp + ".scalePivot")
 
 
 def mk_tree2(itt):
     # create tree base #
-    cmds.polyCylinder(n=f"{PFX}tree2_{itt:02}_base1", r=.7, sx=10, sy=4)  # base 1
+    cmds.polyCylinder(n=f"{PFX}tree2_{itt:02}_base1", r=.7, sx=10, sy=4) # base 1
     # set pivot to base
     cmds.xform(piv=(0, -1, 0))
     cmds.move(0, 1, 0, r=1)
@@ -204,23 +198,23 @@ def mk_tree2(itt):
 
     # create other blocks
     cmds.select(f"{PFX}tree2_{itt:02}_leaf1", f"{PFX}tree2_{itt:02}_leaf2", f"{PFX}tree2_{itt:02}_leaf3")
-    cmds.duplicate()  # leaf 4, 5, 6
+    cmds.duplicate()   # leaf 4, 5, 6
     cmds.move(-5.6, 3.13, 0.9, r=1)
     cmds.select(f"{PFX}tree2_{itt:02}_leaf5")
     cmds.rotate(28, 38, -168, r=1)
 
     cmds.select(f"{PFX}tree2_{itt:02}_leaf4", f"{PFX}tree2_{itt:02}_leaf5", f"{PFX}tree2_{itt:02}_leaf6")
-    cmds.duplicate()  # leaf 7, 8, 9
+    cmds.duplicate() # leaf 7, 8, 9
     cmds.move(4.2, 0, 0.9, r=1)
     cmds.rotate(-48, 0, 0, r=1)
 
     cmds.select(f"{PFX}tree2_{itt:02}_leaf7", f"{PFX}tree2_{itt:02}_leaf8", f"{PFX}tree2_{itt:02}_leaf9")
-    cmds.duplicate()  # leaf 10, 11, 12
+    cmds.duplicate() # leaf 10, 11, 12
     cmds.move(0, 2.8, -1.6, r=1)
     cmds.rotate(45, 0, 12, r=1)
 
     cmds.select(f"{PFX}tree2_{itt:02}_leaf10", f"{PFX}tree2_{itt:02}_leaf11", f"{PFX}tree2_{itt:02}_leaf12")
-    cmds.duplicate()  # leaf 13, 14, 15
+    cmds.duplicate() # leaf 13, 14, 15
     cmds.move(4.4, -3.4, -0.9, r=1)
 
     # refine leaf positions
@@ -234,13 +228,7 @@ def mk_tree2(itt):
     # create shader, apply, and group leaf bits
     leaf_grp = cmds.group(empty=1, n=f"{PFX}tree2_{itt:02}_leaves")
     leaf_obj_color = cmds.shadingNode('standardSurface', n=f"{PFX}tree3_{itt:02}_leaf_shd", asShader=1)
-    
-    # REPLACE WITH RANGE OF VALUES ONCE YOU GET HOME #
-    # randomize leaf hue
-    r_color = random.uniform( 0.07)
-    g_color = random.uniform(.12)
-    b_color = random.uniform(.18)
-    cmds.setAttr(leaf_obj_color + ".baseColor",r_color, g_color, b_color)
+    cmds.setAttr(leaf_obj_color + ".baseColor", 0.07, .12, .18)
 
     for i in range(15):
         leaf = f"{PFX}tree2_{itt:02}_leaf" + str(i + 1)
@@ -249,7 +237,7 @@ def mk_tree2(itt):
         cmds.hyperShade(assign=leaf_obj_color)
 
     # cleanup
-    cmds.select(f"{PFX}tree2_{itt:02}_base", f"{PFX}tree2_{itt:02}_leaves")
+    cmds.select(f"{PFX}tree2_{itt:02}_base",f"{PFX}tree2_{itt:02}_leaves")
     cmds.DeleteHistory()
     cmds.FreezeTransformations()
 
@@ -265,7 +253,7 @@ def mk_tree3(itt):
     cmds.polyCylinder(n=f"{PFX}tree3_{itt:02}_base1", r=.6, sx=10, sy=4)  # base 1
     # set pivot to base
     cmds.xform(piv=(0, -1, 0))
-    cmds.move(0, 1, 0, r=1)
+    cmds.move(0,1,0,r=1)
     cmds.scale(1.3, 2.3, 1.3)
 
     cmds.select(f"{PFX}tree3_{itt:02}_base1.f[0:9]")
@@ -276,11 +264,11 @@ def mk_tree3(itt):
     cmds.polyExtrudeFacet(f"{PFX}tree3_{itt:02}_base1.f[41]", ls=(.8, .8, .8), t=(-1, 6, -1))
 
     # create branches
-    cmds.polyCylinder(n=f"{PFX}tree3_{itt:02}_base2", r=.35, sx=10, sy=4)  # base 2
+    cmds.polyCylinder(n=f"{PFX}tree3_{itt:02}_base2", r=.35, sx=10, sy=4) # base 2
     cmds.move(.89, 13.25, .1)
     cmds.scale(1, 2, 1)
     cmds.rotate(19, 1.6, -29)
-    cmds.duplicate(f"{PFX}tree3_{itt:02}_base2")  # base 3
+    cmds.duplicate(f"{PFX}tree3_{itt:02}_base2") # base 3
     cmds.move(-1.9, -2.3, -.8, r=1)
     cmds.rotate(15, 0, -132)
 
@@ -296,10 +284,9 @@ def mk_tree3(itt):
         cmds.hyperShade(assign=tree_obj_color)
 
     # create leaves #
-    cmds.polySphere(n=f"{PFX}tree3_{itt:02}_leaf1", r=1.5, sx=8, sy=8)  # leaf 1
+    cmds.polySphere(n=f"{PFX}tree3_{itt:02}_leaf1", r=1.5, sx=8, sy=8) # leaf 1
     cmds.move(2.26, 16, .8, r=1)
-    cmds.select(f"{PFX}tree3_{itt:02}_leaf1.f[21]", f"{PFX}tree3_{itt:02}_leaf1.f[29]",
-                f"{PFX}tree3_{itt:02}_leaf1.f[36]")
+    cmds.select(f"{PFX}tree3_{itt:02}_leaf1.f[21]", f"{PFX}tree3_{itt:02}_leaf1.f[29]", f"{PFX}tree3_{itt:02}_leaf1.f[36]")
     cmds.move(-.7, 0, .55, r=1)
     cmds.scale(0.77, 1, 0.77)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf1.f[37:38]", f"{PFX}tree3_{itt:02}_leaf1.f[46:47]")
@@ -310,50 +297,44 @@ def mk_tree3(itt):
     # duplicate the leaf to make a block
     cmds.select(f"{PFX}tree3_{itt:02}_leaf1")
     cmds.duplicate()  # leaf 2
-    cmds.move(0, -2.2, -1.8, r=1)
-    cmds.rotate(0, 0, 30.4)
+    cmds.move(0,-2.2,-1.8,r=1)
+    cmds.rotate(0,0,30.4)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf2")
-    cmds.duplicate()  # leaf 3
-    cmds.move(-.9, 2.3, 0, r=1)
-    cmds.rotate(-9.5, 10.6, -5)
+    cmds.duplicate() # leaf 3
+    cmds.move(-.9,2.3,0,r=1)
+    cmds.rotate(-9.5,10.6,-5)
 
     # create other blocks
     cmds.select(f"{PFX}tree3_{itt:02}_leaf1", f"{PFX}tree3_{itt:02}_leaf2", f"{PFX}tree3_{itt:02}_leaf3")
-    cmds.duplicate()  # leaf 4, 5, 6
+    cmds.duplicate() # leaf 4, 5, 6
     cmds.move(-4.3, -3, -2, r=1)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf4", f"{PFX}tree3_{itt:02}_leaf5", f"{PFX}tree3_{itt:02}_leaf6")
-    cmds.duplicate()  # leaf 7, 8, 9
-    cmds.move(2.3, 2.1, 0, r=1)
-    cmds.rotate(12, 45, 0, r=1)
+    cmds.duplicate() # leaf 7, 8, 9
+    cmds.move(2.3,2.1,0,r=1)
+    cmds.rotate(12,45,0,r=1)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf7", f"{PFX}tree3_{itt:02}_leaf8", f"{PFX}tree3_{itt:02}_leaf9")
-    cmds.duplicate()  # leaf 10, 11, 12
+    cmds.duplicate() # leaf 10, 11, 12
     cmds.move(0, 3.8, 0, r=1)
-    cmds.rotate(12, 45, 30, r=1)
+    cmds.rotate(12, 45, 30,r=1)
 
     # refine leaf positions
     cmds.select(f"{PFX}tree3_{itt:02}_leaf12")
-    cmds.move(-2.4, -3.6, 1.85, r=1)
-    cmds.rotate(0, 0, -83.9, r=1)
+    cmds.move(-2.4,-3.6,1.85,r=1)
+    cmds.rotate(0,0,-83.9,r=1)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf10")
-    cmds.move(-1.4, -2.1, -1.3, r=1)
-    cmds.rotate(-186.8, 0, 0, r=1)
+    cmds.move(-1.4,-2.1,-1.3,r=1)
+    cmds.rotate(-186.8,0,0,r=1)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf11")
-    cmds.move(-.64, 1.2, 2.79, r=1)
-    cmds.rotate(-121.8, 0, 0, r=1)
+    cmds.move(-.64,1.2,2.79,r=1)
+    cmds.rotate(-121.8,0,0,r=1)
     cmds.select(f"{PFX}tree3_{itt:02}_leaf9")
-    cmds.move(2.1, 3.1, 3.94, r=1)
-    cmds.rotate(137.7, -14.4, -11.8, r=1)
+    cmds.move(2.1,3.1,3.94,r=1)
+    cmds.rotate(137.7,-14.4,-11.8,r=1)
 
     # create shader, apply, and group leaf bits
     leaf_grp = cmds.group(empty=1, n=f"{PFX}tree3_{itt:02}_leaves")
     leaf_obj_color = cmds.shadingNode('standardSurface', n=f"{PFX}tree3_{itt:02}_leaf_shd", asShader=1)
-    
-    # REPLACE WITH RANGE OF VALUES ONCE YOU GET HOME #
-    # randomize leaf hue
-    r_color = random.uniform( 0.08)
-    g_color = random.uniform(.16)
-    b_color = random.uniform(.07)
-    cmds.setAttr(leaf_obj_color + ".baseColor",r_color, g_color, b_color)
+    cmds.setAttr(leaf_obj_color + ".baseColor", 0.08, .16, .07)
 
     for i in range(12):
         leaf = f"{PFX}tree3_{itt:02}_leaf" + str(i + 1)
@@ -362,7 +343,7 @@ def mk_tree3(itt):
         cmds.hyperShade(assign=leaf_obj_color)
 
     # cleanup
-    cmds.select(f"{PFX}tree3_{itt:02}_base", f"{PFX}tree3_{itt:02}_leaves")
+    cmds.select(f"{PFX}tree3_{itt:02}_base",f"{PFX}tree3_{itt:02}_leaves")
     cmds.DeleteHistory()
     cmds.FreezeTransformations()
 
@@ -376,24 +357,23 @@ def mk_tree3(itt):
 def lights():
     # create sky
     skydome = cmds.shadingNode('aiSkyDomeLight', asLight=True)
-    physical_sky_shader = cmds.createNode('aiPhysicalSky')
-    cmds.connectAttr(physical_sky_shader + ".outColor", skydome + ".color")
+    physicalSkyShader = cmds.createNode('aiPhysicalSky')
+    cmds.connectAttr(physicalSkyShader + ".outColor", skydome + ".color")
     cmds.setAttr(skydome + ".intensity", .7)
     cmds.rename(skydome, f"{PFX}physicalSky")
 
     # create lights
-    cmds.directionalLight(n=f"{PFX}dirLight1")  # light 1
-    cmds.rotate(-38, 0, 0)
+    cmds.directionalLight(n=f"{PFX}dirLight1") # light 1
+    cmds.rotate(-38,0,0)
     cmds.setAttr(f"{PFX}dirLight1.color", .2, .3, .4)
     cmds.setAttr(f"{PFX}dirLight1.intensity", .7)
 
-    cmds.directionalLight(n=f"{PFX}dirLight2")  # light 2
-    cmds.rotate(-38, 180, 0)
+    cmds.directionalLight(n=f"{PFX}dirLight2") # light 2
+    cmds.rotate(-38,180,0)
     cmds.setAttr(f"{PFX}dirLight1.color", .5, .3, .3)
     cmds.setAttr(f"{PFX}dirLight1.intensity", .5)
 
     return cmds.group(f"{PFX}physicalSky", f"{PFX}dirLight1", f"{PFX}dirLight2", n=f"{PFX}lights")
-
 
 def rand_tree(itt):
     all_trees = [mk_tree1, mk_tree2, mk_tree3]
@@ -414,7 +394,7 @@ if __name__ == "__main__":
         rand_rotation = random.randint(0, 360)
 
         # set seed
-        # random.seed = 10
+        random.seed = 10
 
         # select a random tree
         curr = rand_tree(i)
@@ -423,7 +403,7 @@ if __name__ == "__main__":
         xpos, ypos, zpos = cmds.pointPosition(vert)
         cmds.move(xpos, ypos, zpos)
         cmds.scale(rand_scale, rand_scale, rand_scale)
-        cmds.rotate(0, rand_rotation, 0)
+        cmds.rotate(0,rand_rotation,0)
 
     cmds.group(PFX + "tree?_*_GRP", n=PFX + "forest")
     cmds.select(cl=1)
